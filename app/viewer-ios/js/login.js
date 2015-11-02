a78893d4b2c9b83b613a6f93d4843e34c492e0cd
fuyaode <dan82625@gmail.com>
2015-11-02 17:54:04 +0800

    use framework7 ajax

--- app/viewer-ios/js/login.js
+++ app/viewer-ios/js/login.js
@@ -1,29 +1,19 @@
 // var $$ = Dom7;
 
-$('#login-form').submit(function() {
-  $.ajax({
-      type: 'post',
-      url: '/auth/local/',
-      data: {
-        identifier: $("input[name=identifier]").val(),
-        password: $("input[name=password]").val()
-      },
-      dataType:'json',
-      success:function(data, textStatus, jqXHR){
-        console.log('=== data ==>',data);
-        if(data.status == 'ok'){
-          myApp.closeModal($$(".login-screen"));
-          myApp.addNotification({
-            title: '登入成功',
-            hold: 2000
-          });
-          $$("#showBooksBtn").show();
-          $$("#loginBtn").hide();
-          $$("#forgetPasswordBtn").hide();
-        }else{
-          myApp.alert('請再次確認帳號密碼喔');
-        }
-      },
-  })
-  return false;
+$$('#login-form').on('submitted', function (e) {
+  var xhr = e.detail.xhr; // actual XHR object
+  var data = JSON.parse(e.detail.data);
+  console.log(data);
+  if(data.status == 'ok'){
+    myApp.closeModal($$(".login-screen"));
+    myApp.addNotification({
+      title: '登入成功',
+      hold: 2000
+    });
+    $$("#showBooksBtn").show();
+    $$("#loginBtn").hide();
+    $$("#forgetPasswordBtn").hide();
+  }else{
+    myApp.alert('請再次確認帳號密碼喔');
+  }
 });
