410e68126c9c88f684ef7dbbaf8124c8a63cb15b
fuyaode <dan82625@gmail.com>
2015-11-06 02:33:40 +0800

    remove comment

--- app/viewer-ios/js/bookList.js
+++ app/viewer-ios/js/bookList.js
@@ -17,27 +17,6 @@ $$(document).on('pageInit', '.page[data-page="bookList"]', function (e) {
 function showBookList(data){
   var bookListTemplate = $$('script#booklist').html();
   var compiledBookListTemplate = Template7.compile(bookListTemplate);
-  myApp.template7Data.bookInfo = data ; 
+  myApp.template7Data.bookInfo = data ;
   $$('#bookListUl').html(compiledBookListTemplate(data));
-  // var mainView = myApp.addView('#bookListUl');
-  // console.log("!!!",mainView);
-  // var newPageContent = '';
-  // $$.each(data, function (i, book) {
-  //     newPageContent += '<li class="item-content">'+
-  //       '<div class="item-media"><img src="http://auo.koobe.com.tw/Public/Thumbnail.aspx?ea26fd19-0d44-4380-a10e-2e34ea755aeb&amp;width=800" width="44"></div>'+
-  //       '<div class="item-inner">'+
-  //         '<div class="item-title-row">'+
-  //           '<div class="item-title">'+
-  //             '<a href="book-demo.html" data-context=\''+ JSON.stringify(book) +'\'>'+
-  //               book.name+
-  //             '</a>'+
-  //           '</div>'+
-  //         '</div>'+
-  //         '<div class="item-subtitle">作者：'+ book.author +'</div>'+
-  //         '<div class="item-subtitle">出版社：'+ book.publishingHouse +'</div>'+
-  //       '</div>'+
-  //     '</li>';
-  // });
-  // console.log("??"+newPageContent);
-  // mainView.router.loadContent(newPageContent);
 }
