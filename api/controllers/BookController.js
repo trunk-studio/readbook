a36604b4b11d57cc73089131877fefa067db0eac
fuyaode <dan82625@gmail.com>
2015-11-24 17:53:11 +0800

    add updateCover

--- api/controllers/BookController.js
+++ api/controllers/BookController.js
@@ -44,6 +44,24 @@ module.exports = {
   getBookList: async(req, res) =>{
     try {
       let date = req.body;
+
+      let isShouldUpdateCover = await db.Book.findAll({
+        where:{
+          makingStatus: 2,
+          cover: null
+        },
+        order: 'name',
+        include:{
+          model: db.Site,
+          where:{
+            id: date.Site.id
+          }
+        }
+      });
+
+      if(isShouldUpdateCover.length > 0)
+        await BookService.updateCover(isShouldUpdateCover);
+
       let books = await db.Book.findAll({
         where:{
           makingStatus: 2
@@ -56,7 +74,8 @@ module.exports = {
           }
         }
       });
-      sails.log.info("=== booksList ===",books);
+
+      // sails.log.info("=== booksList ===",books);
       return res.ok(books);
     } catch (e) {
       sails.log.error(e);
