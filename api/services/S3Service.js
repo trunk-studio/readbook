bbb476ae35aa526282d7e97742cdda897d91ec06
fuyaode <dan82625@gmail.com>
2015-11-25 18:27:41 +0800

    fix s3

--- api/services/S3Service.js
+++ api/services/S3Service.js
@@ -4,7 +4,7 @@ module.exports = {
 
   getS3Url: async() => {
     try {
-      let bucket = sig.urlSigner(sails.config.S3.id, sails.config.S3.key);
+      let bucket = sig.urlSigner(sails.config.S3.id, sails.config.S3.key, sails.config.S3.options);
       let url = bucket.getUrl('GET', '/cover/0/1/2/01200fe3-5fb0-459f-99ad-afe4e493c363/01200fe3-5fb0-459f-99ad-afe4e493c363.jpg', 'koobe-e7read', 10);
       return url;
     } catch (e) {
