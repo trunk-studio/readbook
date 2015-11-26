0fad4a1bc3aa7a51bc9d8543fab762ad9b531eab
fuyaode <dan82625@gmail.com>
2015-11-26 14:16:31 +0800

    use s3 cover & book

--- test/unit/services/S3Service.spec.js
+++ test/unit/services/S3Service.spec.js
@@ -2,7 +2,7 @@ describe("about S3Service =>", () => {
 
   it.only('get S3 url', async (done) => {
     try{
-      let url = await S3Service.getS3Url('/cover/0/1/2/01200fe3-5fb0-459f-99ad-afe4e493c363/01200fe3-5fb0-459f-99ad-afe4e493c363.jpg');
+      let url = await S3Service.getS3Url('/book/c/7/7/c774b6f8-60c1-43de-a268-400871308b23/cover.jpg');
       console.log(url);
       done();
     } catch (e) {
