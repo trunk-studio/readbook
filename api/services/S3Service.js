1020de41e2c4b9a0220cd37ff7ad0391db7dcb11
lyhcode <lyhcode@gmail.com>
2016-01-12 20:46:06 +0800

    update bucket name

--- api/services/S3Service.js
+++ api/services/S3Service.js
@@ -5,7 +5,7 @@ module.exports = {
   getS3Url: async(path,time) => {
     try {
       let bucket = sig.urlSigner(sails.config.S3.id, sails.config.S3.key, sails.config.S3.options);
-      let url = await bucket.getUrl('GET', path, 'koobe-e7read-new', time);
+      let url = await bucket.getUrl('GET', path, 'koobe-e7read', time);
       return url;
     } catch (e) {
       return console.error(e.stack)
